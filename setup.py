baae90ac4b3b57b4827207438847d809348e4f04
Corey Smith <coreys@uchicago.edu>
2019-04-18 08:38:26 -0500

    initial git commit with cookiecutter data science template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='looking through instacart published transactional data',
+    author='Corey Smith',
+    license='MIT',
+)
