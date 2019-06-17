e6a827e53bb9203919d5091523f3dedd122d86c6
Corey Smith <coreys@uchicago.edu>
2019-06-17 03:38:04 +0000

    improve database connection and function, create smaller prototype csv files

---
+++ src/data/create_small_prototype_csv_files.py
@@ -0,0 +1,91 @@
+"""
+Create two small prototype csv files for use when creating 
+and testing analysis and visualization functions.
+
+Copyright (c) 2019, Corey Smith
+Distributed under the MIT License.
+See LICENCE file for full terms.
+"""
+
+import os
+import pandas as pd
+from database_connection import Database
+
+project_dir = os.path.join(os.path.dirname('__file__'), os.pardir)
+dotenv_path = os.path.join("../" + project_dir, '.env')
+
+db = Database(dotenv_path)
+
+# Make weighted adjacency matrix from the instacart database
+query_string = """
+    SELECT 
+        CAST (o.user_id AS INTEGER), 
+        p.product_id, 
+        COUNT(p.product_id) AS weight
+    FROM 
+        order_products__prior as p
+    INNER JOIN
+        (
+            SELECT 
+                order_id, 
+                user_id 
+            FROM 
+                orders 
+            WHERE 
+                eval_set='prior'
+            LIMIT 
+                10000
+        ) AS o
+    ON 
+        o.order_id=p.order_id
+    GROUP BY
+        o.user_id, p.product_id
+    ;
+    """
+df = db.get_dataframe(query_string)
+df.to_csv("../../data/interim/small_10000_orders_weighted_adjacency_matrix.csv",
+          index=False)
+
+# Ouery the database to create a table of 10,000 prior orders to use for prototyping functions.
+prior_orders_query = """
+    SELECT
+        o.user_id, p.product_id, 
+        products.department_id, products.aisle_id, 
+        p.order_id, p.add_to_cart_order, 
+        p.reordered, o.order_number, 
+        o.order_dow, o.order_hour_of_day, 
+        CAST (o.days_since_prior AS INTEGER)
+    FROM
+        order_products__prior AS p
+    INNER JOIN
+    (
+        SELECT
+            user_id, order_id, order_number, 
+            order_dow, order_hour_of_day, 
+            days_since_prior
+        FROM 
+            orders
+        WHERE
+            order_id IN 
+            (
+                SELECT 
+                    DISTINCT order_id 
+                FROM 
+                    orders 
+                WHERE 
+                    eval_set='prior'
+                LIMIT 
+                    10000
+            )
+    ) AS o
+    ON 
+        o.order_id=p.order_id
+    INNER JOIN
+        products
+    ON
+        products.product_id=p.product_id
+    ;
+"""
+
+df = db.get_dataframe(prior_orders_query)
+df.to_csv("../../data/interim/prior_10000_orders_full_info.csv", index=False)
---
+++ src/data/database_connection.py
@@ -0,0 +1,92 @@
+"""
+Create connections to a postgres database either locally or remotly
+using python's sqlalchemy.
+
+
+Copyright (c) 2019, Corey Smith
+Distributed under the MIT License.
+See LICENCE file for full terms.
+"""
+import os
+import urllib.parse
+
+import dotenv
+from eralchemy import render_er
+import pandas as pd
+from sqlalchemy import MetaData, create_engine
+
+
+class Database:
+    """
+    Database is a class used to connect to a relational database using sqlalchemy
+    and an env file containing the database credentials.
+    
+    Some more info about the class attributes and functions.
+    """
+
+    def __init__(self, dotenv_path):
+        """
+        Initialize the database class with the path to the env file containing
+        the database credentials.
+        """
+        _ = dotenv.load_dotenv(dotenv_path=dotenv_path)
+
+        DB_PASSWORD = os.getenv("DB_PASSWORD", "")
+        DB_NAME = os.getenv("DB_NAME")
+        DB_USER = os.getenv("DB_USER", "postgres")
+        DB_HOST = os.getenv("DB_HOST", "")
+
+        self.db_string = (
+            f"postgresql+psycopg2://{DB_USER}:"
+            f"{urllib.parse.quote_plus(DB_PASSWORD)}@{DB_HOST}/{DB_NAME}"
+        )
+        self.engine = create_engine(self.db_string)
+        self.conn = self.engine.connect()
+
+        self.metadata = MetaData()
+        self.metadata.reflect(bind=self.engine)
+
+        self.tables = self.metadata.tables
+        self.table_names = self.tables.keys()
+
+    def table(self, table_name: str):
+        """
+        Get a specific table object from the database.
+        """
+        return self.tables[table_name]
+
+    def get_dataframe(self, query):
+        """
+        Execute the query and return a pandas dataframe.
+        """
+        result_proxy = self.conn.execute(query)
+        headings = result_proxy.keys()
+        data = []
+        for row in result_proxy:
+            data.append(row.values())
+
+        return pd.DataFrame(data, columns=headings)
+
+    def define_instacart_db(self):
+        """
+        Add the instacart database tables as attributes of the Database class.
+        """
+        self.departments = self.tables["departments"]
+        self.orders = self.tables["orders"]
+        self.aisles = self.tables["aisles"]
+        self.order_products__prior = self.tables["order_products__prior"]
+        self.products = self.tables["products"]
+        self.order_products__train = self.tables["order_products__train"]
+        self.orders_test = self.tables["orders_test"]
+        self.orders_no_tests = self.tables["orders_no_tests"]
+#         self.customer_products = self.tables["customer_products"]
+
+    def save_layout(self, filename):
+        """
+        Save the layout of the database to file.
+        
+        Allowed filetypes are png, dot, er (markdown), and pdf.
+        """
+        render_er(self.db_string, filename)
+        
+        
