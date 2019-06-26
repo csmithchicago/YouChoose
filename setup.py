b86a5a31dd003d7ffe9b544474d9dc15f08bfe09
Corey Smith <coreys@uchicago.edu>
2019-06-26 18:44:23 +0000

    formatting changes from pre-commit hooks being added

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import find_packages, setup
+from setuptools import setup, find_packages
 
 setup(
-    name='src',
+    name="src",
     packages=find_packages(),
-    version='0.1.0',
-    description='looking through instacart published transactional data',
-    author='Corey Smith',
-    license='MIT',
+    version="0.1.0",
+    description="looking through instacart published transactional data",
+    author="Corey Smith",
+    license="MIT",
 )
