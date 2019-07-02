73591c2507b32dd662c7c70a4547db111303b510
Corey Smith <coreys@uchicago.edu>
2019-07-02 09:32:15 -0500

    increase patch number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setup(
     name="youchoose",
     packages=find_packages(),
-    version="0.1.0",
+    version="0.1.1",
     description="YouChoose is an open source recommendation library built on PyTorch.",
     long_description=long_description,
     long_description_content_type="text/markdown",
